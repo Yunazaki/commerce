ebd2168963c387a2dadef98b6867e482b29337dd
Euna <66821940+Yunazaki@users.noreply.github.com>
2024-01-10 09:02:56 -0700

    Update admin.py
    Shows id, title, and bid in admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import User, Auctions
 
+class AuctionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'bid')
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Auctions)
+admin.site.register(Auctions, AuctionsAdmin)
