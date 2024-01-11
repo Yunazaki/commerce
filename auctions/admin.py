8aa1cf40010728e16fdf1f928da77ad11f9b52a6
Euna <66821940+Yunazaki@users.noreply.github.com>
2024-01-10 21:18:59 -0700

    Add buttons to listing_page.html

--- auctions/admin.py
+++ auctions/admin.py
@@ -5,7 +5,7 @@ class AuctionsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'bid')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username')
+    list_display = ('username', 'id')
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
