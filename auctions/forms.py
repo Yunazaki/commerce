fe13247f007619ff0238181b3203d22baf6b1d01
Euna <66821940+Yunazaki@users.noreply.github.com>
2024-01-12 08:11:15 -0700

    Exclude is_active from forms.py

--- auctions/forms.py
+++ auctions/forms.py
@@ -4,4 +4,4 @@ from .models import Auctions
 class NewListingForm(forms.ModelForm):
     class Meta:
         model = Auctions
-        exclude = ["date_listed", "user"]
+        exclude = ["date_listed", "user", "is_active"]
